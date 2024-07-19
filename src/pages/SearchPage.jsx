ad2c0e5a244bf4015f2806d958c204b0646632c3
Ronny <ronnyd.1994@gmail.com>
2024-07-19 16:26:08 -0500

    funciona en Local

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -5,11 +5,12 @@ import { CardPokemon } from '../components'
 
 export const SearchPage = () => {
     const location = useLocation()
-
+console.log(Location);
     const { globalPokemons } = useContext(PokemonContext)
 
     const filteredPokemons = globalPokemons.filter(pokemon => pokemon.name.includes(location.state
         .toLowerCase()))
+      
     return (
         <div className='container'>
             <p className='p-search'>
